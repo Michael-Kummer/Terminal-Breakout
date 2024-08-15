392f3090fd5cd778a2ef31704d59a89e90e8724f
Michael Kummer <mkummer@ualberta.ca>
2024-08-15 06:37:51 +0000

    Added the ball movement + basic side collision

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 If your terminal has python you can play breakout entirely in it.
 
-Currently it only draws the blocks, ball, and paddle.
+## Currently it only draws the blocks, ball, and paddle.
+
+TODO list:
+- Create a velocity system for the ball
+- Implement a collision system between the ball and blocks + ball and screen sides
+- Implement a loss system - default the loss to shutting down the game for now
+- Ensure that any unexpected exits do not leave the curses library in an awkward state causing the user to have to "reset" their terminal
+- Implement colour for the ball + blocks
+
+
 
 
--- breakout.py
+++ breakout.py
@@ -12,15 +12,19 @@ class Breakout:
         Inputs: None
         Returns: None
         '''
+        #Curses init stuff
         curses.noecho()
         self.stdscr = stdscr
         curses.curs_set(0)
+        #Setting heights and sizes
         self.max_height, self.max_width = stdscr.getmaxyx()
-        self.paddle = Paddle(self.max_width//2, self.max_height - 2, 10 ,stdscr)
-        self.ball = Ball(self.max_width // 2, self.max_height // 2, stdscr)
+        self.paddle_size = 20
+        self.paddle = Paddle(self.max_width//2, self.max_height - 2, self.paddle_size ,stdscr)
+        self.ball = Ball(self.max_width // 3, self.max_height // 2,0, stdscr)
         self.block = Block(stdscr)
         self.block_width = 10
 
+        #initalizing block grid
         self.grid_width = self.max_width // self.block_width
         self.grid_height = 4
         self.grid = [[0 for _ in range(self.grid_width)] for _ in range(self.grid_height)]
@@ -35,10 +39,21 @@ class Breakout:
 
     def draw_ball(self):
         '''
+        TODO: create a self.xvelocity and self.yvelocity. Spawns going 45* down to paddle in the middle
         Spawns ball
         Inputs: None
         Returns: None
         '''
+        if self.ball.yvelocity > 0:
+            self.ball.move_up() 
+        elif self.ball.yvelocity < 0:
+            self.ball.move_down()
+       
+        if self.ball.xvelocity > 0:
+            self.ball.move_right()
+        elif self.ball.xvelocity < 0:
+            self.ball.move_left()
+        
         self.ball.draw(self.stdscr)    
 
     def draw_blocks(self):
@@ -57,9 +72,12 @@ class Breakout:
         '''
         Checks collision between ball, paddle, brick
         Inputs: None
-        Returns: ball->paddle, ball->brick, ball->screensides
+        Returns: None
         '''  
-        pass
+        if self.ball.x == 0 or self.ball.x == self.max_width-2:
+            self.ball.xvelocity *= -1
+        if self.ball.y == 0 or self.ball.y == self.max_height-1:
+            self.ball.yvelocity *= -1
 
     def run(self):
         '''
@@ -82,7 +100,8 @@ class Breakout:
                 self.paddle.move_right()
             elif key == ord('q'):
                 break
-            
+           
+            self.check_collision()
             self.draw_blocks()
             self.stdscr.clear()
             self.draw_paddle()
@@ -96,15 +115,13 @@ class Block:
 
     def draw(self, x, y, block_width):
         max_height, max_width = self.stdscr.getmaxyx()
-        for i in range(2):  # Block height
-            for j in range(block_width):  # Block width
-                # Check if the position is within screen bounds
+        for i in range(2): 
+            for j in range(block_width): 
                 if 0 <= y + (i - 1) < max_height and 0 <= x + j < max_width:
                     try:
                         self.stdscr.addch(y + (i - 1), x + j, curses.ACS_BLOCK)
                     except curses.error:
-                        pass  # Ignore errors to prevent crashing
-
+                        pass  
 
 class Paddle:
     def __init__(self,x,y,width,stdscr):
@@ -115,20 +132,27 @@ class Paddle:
 
     def draw(self,stdscr):
         for i in range(self.width):
-            stdscr.addch(self.y,self.x+i,curses.ACS_CKBOARD)
+            if self.x + i < self.max_width:
+                stdscr.addch(self.y,self.x+i,curses.ACS_CKBOARD)
 
     def move_left(self):
         if self.x > 0:
-            self.x -= 1
+            self.x -= 3
 
     def move_right(self):
         if self.x + self.width < self.max_width:
-            self.x +=1
+            self.x += 3
 
 class Ball:
-    def __init__(self,x,y,stdscr):
+    def __init__(self,x,y,velocity,stdscr):
         self.x = x
         self.y = y
+        self.velocity = velocity
+       
+       #ball velocities
+        self.xvelocity = 1
+        self.yvelocity = -1
+
         self.max_height, self.max_width = stdscr.getmaxyx()
 
     def draw(self,stdscr):
@@ -147,9 +171,8 @@ class Ball:
             self.x -= 1
 
     def move_right(self):
-        if self.x != self.max_width:
+        if self.x <= self.max_width:
             self.x += 1
-
 
 def main(stdscr):
     breakout = Breakout(stdscr)
