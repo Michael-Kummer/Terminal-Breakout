a23940993e13a4423d59e5024c50d39585acd081
Michael Kummer <mkummer@ualberta.ca>
2024-08-16 04:28:48 +0000

    Added a broken collision system + cool Ascii art

--- breakout.py
+++ breakout.py
@@ -1,5 +1,9 @@
 # ----------------------------------------------------
-# Breakout Game
+#
+# /_  __/__  _________ ___  (_)___  ____ _/ /  / __ )________  ____ _/ /______  __  __/ /_
+#  / / / _ \/ ___/ __ `__ \/ / __ \/ __ `/ /  / __  / ___/ _ \/ __ `/ //_/ __ \/ / / / __/
+# / / /  __/ /  / / / / / / / / / / /_/ / /  / /_/ / /  /  __/ /_/ / ,< / /_/ / /_/ / /_  
+#/_/  \___/_/  /_/ /_/ /_/_/_/ /_/\__,_/_/  /_____/_/   \___/\__,_/_/|_|\____/\__,_/\__/                                                            
 #
 # Author: Michael
 # ----------------------------------------------------
@@ -35,8 +39,8 @@ class Breakout:
         self.grid = [
             [0 for _ in range(self.grid_width)] for _ in range(self.grid_height)
         ]
-        
-        #Initalize time variable
+
+        # Initalize time variable
         self.current_time = time.time()
 
     def draw_paddle(self):
@@ -49,7 +53,8 @@ class Breakout:
 
     def draw_ball(self):
         """
-        TODO: create a self.xvelocity and self.yvelocity. Spawns going 45* down to paddle in the middle
+        TODO: create a self.xvelocity and self.yvelocity.
+            Spawns going 45* down to paddle in the middle
         Spawns ball
         Inputs: None
         Returns: None
@@ -63,7 +68,7 @@ class Breakout:
             self.ball.move_right()
         elif self.ball.xvelocity < 0:
             self.ball.move_left()
-        
+
         self.ball.draw(self.stdscr)
 
     def draw_blocks(self):
@@ -78,11 +83,9 @@ class Breakout:
                 y = i * 4
                 self.block.draw(x, y, self.block_width)
 
-
-
     def check_collision(self):
         """
-        Checks collision between ball, paddle, brick
+        Checks collision between ball, paddle, block
         Inputs: None
         Returns: None
         """
@@ -91,15 +94,49 @@ class Breakout:
             self.ball.xvelocity *= -1
         if self.ball.y == 0 or self.ball.y == self.max_height - 1:
             self.ball.yvelocity *= -1
-
+        
+        # Block Collision
+        self.block_collision()
+        
         # Paddle Collision
+        self.map = [self.paddle.x + i for i in range(self.paddle.width)]
         if self.ball.y == self.paddle.y - 1:
-            for i in range(self.paddle_size):
-                if (
-                    self.ball.x <= self.paddle.x + i
-                    and self.ball.y == self.paddle.y + 1
-                ):
+            for pos in self.map:
+                if self.ball.x == pos:
                     self.ball.yvelocity *= -1
+                    break
+
+    def block_collision(self):
+        blockxcount = self.max_width // (self.block.block_width + 2)
+        block_width = self.block.block_width
+        block_height = 1  # Assuming block height is 1
+
+        for blocknumber, status in self.block.map.items():
+            if status == 1:  # Only check active blocks
+                # Calculate block's position (block_x, block_y) based on its number
+                block_x = (blocknumber % blockxcount) * (block_width + 2)
+                block_y = (blocknumber // blockxcount) * block_height
+
+                # Check if the ball is within the block's boundaries
+                if block_x <= self.ball.x <= block_x + block_width and \
+                   block_y <= self.ball.y <= block_y + block_height:
+                    # Collision detected, deactivate block and bounce
+                    self.block.map[blocknumber] = 0
+                    self.ball.yvelocity *= -1
+
+
+        '''
+            def block_collision (self):
+                for blocknumber in self.block.map.keys():
+                    
+                    if self.block.map[blocknumber] == 1:
+                            if self.map[blocknumber] == 1:
+                        for j in range(self.block_width):
+                            if 0 <= self.block.y < self.max_height and 0 <= self.block.x + j < self.max_width:
+                       #turn off block and bounce 
+                        self.block.map[blocknumber] = 0
+                        self.ball.yvelocity *= -1
+        '''
 
     def run(self):
         """
@@ -108,7 +145,7 @@ class Breakout:
         Returns: None
         """
         self.stdscr.nodelay(True)
-        
+
         while True:
             key = self.stdscr.getch()
 
@@ -121,15 +158,15 @@ class Breakout:
                 break
 
             # Time-based Game Logic
-            if time.time() - self.current_time > 1/30:
+            if time.time() - self.current_time > 1 / 20:
                 self.stdscr.clear()  # Clear the screen once
                 self.check_collision()  # Handle ball collisions
-                
+
                 # Redraw game components
                 self.draw_paddle()
                 self.draw_ball()
                 self.draw_blocks()
-                
+
                 self.stdscr.refresh()  # Refresh the screen after all draws
 
                 # Reset the timer
--- objects.py
+++ objects.py
@@ -1,26 +1,60 @@
-import curses
+"""
+Objects.py plugs into breakout.py
 
+Block:
+    - Create a map in dictionary for each block + status
+    - Map is actively edited by breakout.check_collision during runtime
+    - Draw blocks
+
+Paddle:
+    - Moves paddle left and right.
+    - Defaults to the bottom of the screen
+
+Ball:
+    - Moves ball in all 4 directions
+    - breakout.check_collision changes xvelocity and yvelocity during runtime
+    - Draws ball
+"""
+
+import curses
 
 class Block:
     def __init__(self, stdscr):
         self.stdscr = stdscr
+        self.max_height, self.max_width = self.stdscr.getmaxyx()
+        self.map = {}
+        self.block_width = 4
+        self.block_height = 1  # Define the height of a block
+        self.spawn_map()
+
+    def spawn_map(self):
+        blockxcount = self.max_width // (self.block_width + 2)
+        blockycount = 4
+
+        for j in range(blockycount):
+            for i in range(blockxcount):
+                blocknumber = (j * (i - 1)) + i
+                self.map[blocknumber] = 1  # Use 1 to mark active blocks
 
     def draw(self, x, y, block_width):
-        max_height, max_width = self.stdscr.getmaxyx()
-        for i in range(2):
-            for j in range(block_width):
-                if 0 <= y + (i - 1) < max_height and 0 <= x + j < max_width:
-                    try:
-                        self.stdscr.addch(y + (i - 1), x + j, curses.ACS_BLOCK)
-                    except curses.error:
-                        pass
-
-
+        """
+        Draws line by line. checks if block is active
+        Inputs: block x pointer, block y pointer, block width
+        """
+        for blocknumber in self.map.keys():
+            if self.map[blocknumber] == 1:  # Check if block is active
+                for j in range(block_width):
+                    if 0 <= y < self.max_height and 0 <= x + j < self.max_width:
+                        try:
+                            self.stdscr.addch(y, x + j, "█")
+                        except curses.error:
+                            pass
 class Paddle:
     def __init__(self, x, y, width, stdscr):
         self.x = x
         self.y = y
         self.width = width
+        self.map = [0] * self.width
         self.max_height, self.max_width = stdscr.getmaxyx()
 
     def draw(self, stdscr):
@@ -29,7 +63,7 @@ class Paddle:
                 stdscr.addch(self.y, self.x + i, curses.ACS_CKBOARD)
 
     def move_left(self):
-        if self.x > 0:
+        if self.x > 3:
             self.x -= 3
 
     def move_right(self):
